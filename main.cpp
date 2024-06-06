e577bc68a270b1e84af5ed292ee125cc3f10aa79
Max <maxstill2006@gmail.com>
2024-06-06 19:13:56 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int stepin(int a, int b, int i = 0, int result = 1) {
+    if (i < b) {
+        return stepin(a, b, ++i, result * a);
+    }
+    else {
+        return result;
+    }
+}
 
+int main() {
+    cout << stepin(2, 3) << endl;
     return 0;
 }
