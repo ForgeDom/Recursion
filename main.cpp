93cf9dc7bb0708d458894a31afae87f0427000ad
ForgeDom <maxstill2006@gmail.com>
2024-06-06 21:01:47 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int func(int a, int b) {
+    if (b == 0) {
+        return a; 
+    }
+    return func(b, a % b); 
+}
+
 int main() {
+    int num1, num2;
+    cin >> num1;
+    cin >> num2;
+
+    cout << func(num1, num2) << endl;
 
     return 0;
 }
