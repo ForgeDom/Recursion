7bcb269691edbae62ecd6830966f817c104b1225
Max <maxstill2006@gmail.com>
2024-06-06 19:34:12 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int Calcul(int a, int b, int sum = 0) {
+    if (a <= b) {
+        sum += a;
+        return Calcul(++a, b, sum);
+    }
+    else {
+        return sum;
+    }
+}
 int main() {
-
+    int a, b;
+    cin >> a >> b;
+    cout << Calcul(a, b) << endl;
     return 0;
 }
