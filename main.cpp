1abdc0c86de6576dbea3e773b48cf57b4dbf446f
Max <maxstill2006@gmail.com>
2024-06-06 19:24:07 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void Draw(int N) {
+    if (N > 0) {
+        cout << '*';
+        Draw(N - 1);
+    }
+}
+
 int main() {
+    int N;
+    cout << "Enter stars amount: ";
+    cin >> N;
+
+    Draw(N);
+    cout << endl;
 
     return 0;
 }
